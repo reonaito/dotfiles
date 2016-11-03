f6dff1adf406f4593f9a64692e3eb44e87d93f2d
Reona Ito <reo@MacBook.local>
2016-11-03 22:15:59 +0900

    commit

---
+++ vim/scripts/vim_win_desktop.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /d %~dp0
+
+mklink "%VIM%\_vimrc" "%CD%\src\_vimrc_win"
+mklink "%VIM%\vimrc_common.vim" "%CD%\src\vimrc_common.vim"
+mklink "%VIM%\vimrc_win.vim" "%CD%\src\vimrc_win.vim"
+mklink "%VIM%\_gvimrc" "%CD%\src\_gvimrc_win_desktop"
+mklink "%VIM%\gvimrc_win.vim" "%CD%\src\gvimrc_win.vim"
+mklink "%VIM%\gvimrc_win_desktop.vim" "%CD%\src\gvimrc_win_desktop.vim"
+
